9de81f2949c84b85f0aeeb1200bbe6200028d4ff
Matteoo98 <matteo.molteni@studenti.unicam.it>
2020-10-23 11:16:39 +0200

    crawl

--- ProgettoLube/backend/webinspector/webinspector/src/main/java/com/lube/webinspector/Crawler/Crawler.java
+++ ProgettoLube/backend/webinspector/webinspector/src/main/java/com/lube/webinspector/Crawler/Crawler.java
@@ -2,13 +2,20 @@ package com.lube.webinspector.Crawler;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.io.Files;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import edu.uci.ics.crawler4j.crawler.Page;
 import edu.uci.ics.crawler4j.crawler.WebCrawler;
@@ -25,8 +32,7 @@ import edu.uci.ics.crawler4j.url.WebURL;
 public class Crawler extends WebCrawler {
 
     private static final Pattern filters = Pattern.compile(
-        ".*(\\.(css|js|mid|mp2|mp3|mp4|wav|avi|mov|mpeg|ram|m4v|pdf" +
-        "|rm|smil|wmv|swf|wma|zip|rar|gz))$");
+            ".*(\\.(css|js|mid|mp2|mp3|mp4|wav|avi|mov|mpeg|ram|m4v|pdf" + "|rm|smil|wmv|swf|wma|zip|rar|gz))$");
 
     private static final Pattern imgPatterns = Pattern.compile(".*(\\.(bmp|gif|jpe?g|png|tiff?))$");
 
@@ -41,37 +47,50 @@ public class Crawler extends WebCrawler {
     @Override
     public boolean shouldVisit(Page referringPage, WebURL url) {
         String href = url.getURL().toLowerCase();
-
-        //TODO: HO COMMENTATO COSI VISITA SOLO IL SEED
-
-       /* if (filters.matcher(href).matches()) {
+        
+        // TODO: HO COMMENTATO COSI VISITA SOLO IL SEED
+        // filtro
+        if (filters.matcher(href).matches()) {
+            System.out.println("No Visit "+url.getURL());
             return false;
         }
 
-        //per le immagini
+        // per le immagini
         if (imgPatterns.matcher(href).matches()) {
+            System.out.println("To Visit "+url.getURL());
             return true;
         }
+        // sotto domini
 
-        for (String domain : crawlDomains) {
+        /*for (String domain : crawlDomains) {
             if (href.startsWith(domain)) {
                 return true;
             }
         }*/
+
         return false;
     }
 
     @Override
     public void visit(Page page) {
         String url = page.getWebURL().getURL();
-        String keysearch="Camerino";
+        String keysearch = "Lube";
         // TO SEARCH ON SITE
         System.out.println("URL: " + url);
 
         if (page.getParseData() instanceof HtmlParseData) {
-            HtmlParseData htmlParseData = (HtmlParseData) page.getParseData();      
-            String text = htmlParseData.getText(); //extract text from page
-            String html = htmlParseData.getHtml(); //extract html from page
+            HtmlParseData htmlParseData = (HtmlParseData) page.getParseData();
+            String text = htmlParseData.getText(); // extract text from page
+            String html = htmlParseData.getHtml(); // extract html from page
+            Document doc = Jsoup.parseBodyFragment(html);
+            // jsoup to search img tag
+            Elements img = doc.getElementsByTag("img");
+            ArrayList<String> urlTagImg = new ArrayList<String>();
+            for (Element el : img) {
+                urlTagImg.add("https://www.lubecreostorepratolapeligna.it/"+el.attr("src"));
+            }
+            //urlTagImg.forEach(string->{System.out.println(string);});
+            System.out.println("size : "+urlTagImg.size());
             Set<WebURL> links = htmlParseData.getOutgoingUrls();
 
             System.out.println("---------------------------------------------------------");
@@ -81,18 +100,26 @@ public class Crawler extends WebCrawler {
             System.out.println("Number of outgoing links: " + links.size());
             System.out.println("---------------------------------------------------------");
 
-            if(text.contains(keysearch) || html.contains(keysearch)){
+            /** CERCA TAG IMG */
+            /*
+             * Pattern pattern = Pattern.compile(
+             * "(<img\\b|(?!^)\\G)[^>]*?\\b(src|width|height)=([\"']?)([^\"]*)\\3"); Matcher
+             * matcher = pattern.matcher(html); while (matcher.find()) { if
+             * (!matcher.group(1).isEmpty()) { // We have a new IMG tag
+             * System.out.println("\n--- NEW MATCH ---"); }
+             * System.out.println(matcher.group(2) + ": " + matcher.group(4)); }
+             */
+            if (text.contains(keysearch) || html.contains(keysearch)) {
                 System.out.println("KEYWORD MATCHED");
-            }else{
+            } else {
                 System.out.println("KEYWORD NOT MATCHED");
             }
         }
-        //TO STORE IMAGES
+        // TO STORE IMAGES
 
         // We are only interested in processing images which are bigger than 10k
-        /*if (!imgPatterns.matcher(url).matches() ||
-            !((page.getParseData() instanceof BinaryParseData) ||
-              (page.getContentData().length < (10 * 1024)))) {
+        if (!imgPatterns.matcher(url).matches() || !((page.getParseData() instanceof BinaryParseData)
+                || (page.getContentData().length < (10 * 1024)))) {
             return;
         }
 
@@ -107,7 +134,7 @@ public class Crawler extends WebCrawler {
             WebCrawler.logger.info("Stored: {}", url);
         } catch (IOException iox) {
             WebCrawler.logger.error("Failed to write file: {}", filename, iox);
-        }*/
+        }
     }
 
 }
