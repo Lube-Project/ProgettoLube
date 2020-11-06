b41fad97d0aa4b8aa1a481bf34bf5debaff0ac6b
Matteoo98 <matteo.molteni1998@gmail.com>
2020-11-06 15:47:17 +0100

    load siti

---
+++ ProgettoLube/WebInspector/LoadResources.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+# METODO per ottenere la lista dei siti da analizzare
+def loadsiti():
+    df = pd.read_excel('filename.xlsm', sheetname=0)  # can also index sheet by name or fetch all sheets
+    mylist = df['column name'].tolist()
+    return mylist
