968082d260404ba7ef5cb99394a38fa91953cd1c
tommaso <tommaso.cippitelli@studenti.unicam.it>
2020-12-26 11:05:29 +0100

    pyquery

--- ProgettoLube/WebInspector/DBmanager.py
+++ ProgettoLube/WebInspector/DBmanager.py
@@ -55,11 +55,47 @@ class DBmanager:
     # return year(x) average reports
     def retrieve_year_average(self, collection, year):
         # TODO: ritornare tutti i report medi dell'anno x ogni sito
-        pass
+        #Attualmente: Nel db ho inserito tutte macchine, la media è sull'anno all'interno dell'oggetto obj
+        lista = []
+        for z in collection.aggregate([
+            {
+                "$project": {
+                    "year": {"$year": "$date"},
+                    "date": 1
+                }
+            },
+            {"$match": { "year": year}},
+            {
+        "$group": {
+            "_id": "mediaAnnoZ",
+            "avgValue": {"$avg": "$average"},
+        }
+            }
+        ]):
+            lista.append(z)
 
     # return month(x) of the year(x) average reports
     def retrieve_month_year_average(self, collection, year, month):
         # TODO: ritornare tutti i report medi del mese x dell'anno x per ogni sito
+        #Attualmente: Nel db ho inserito tutte macchine, la media è sull'anno all'interno dell'oggetto obj
+        lista = []
+        for z in collection.aggregate([
+            {
+                "$project": {
+                    "month": {"$month": "$date"},
+                    "year": {"$year": "$date"},
+                    "date": 1
+                }
+            },
+            {"$match": {"month": month, "year": year}},
+            {
+                "$group": {
+                    "_id": "mediaMeseXAnnoZ",
+                    "avgValue": {"$avg": "$average"},
+                }
+            }
+        ]):
+            lista.append(z)
         pass
 
     # return day(x) of month(x) of the year(x) average reports
@@ -67,6 +103,10 @@ class DBmanager:
         # TODO: ritornare tutti i report del giorno x del mese x dell'anno x per ogni sito
         lista = []
         x = datetime.date(year, month, day)
-        for x in collection.find({"date": x}, {"_id": 0, "date": 1, "report": 1}):
-            lista.append(x)
+        #datetime.date non si può salvare in pymongo, quindi nell'oggetto Date abbiamo anche l'orario
+        for z in collection.find({"date": {'$gte': x, '$lt': x + datetime.timedelta(days=1)}},
+                         {"_id": 0, "date": 1, "report": 1}):
+            lista.append(z)
+        #for x in collection.find({"date": x}, {"_id": 0, "date": 1, "report": 1}):
+         #   lista.append(x)
         return lista
