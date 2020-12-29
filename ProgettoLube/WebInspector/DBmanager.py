c58f64e2d2bd682b27da45ad7b823aa5afd717a8
tommaso <tommaso.cippitelli@studenti.unicam.it>
2020-12-29 16:08:29 +0100

    DBmanager

--- ProgettoLube/WebInspector/DBmanager.py
+++ ProgettoLube/WebInspector/DBmanager.py
@@ -1,4 +1,7 @@
-# TODO: connessione con vero database forse mongodb e per testing mongocloud
+# TODO: RICORDAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA che il nome in input ai metodi va controllato lato frontend dandogli al medesimo la tabella excel
+        #TODO:FARE menu a tendina dove mostri tutti i nomi dei siti, così non bisogna controllare la validità del nome
+#TODO: RICORDAAA DI CONTROLLARE IL RANGE DELLA VALUTAZIONE LATO FRONTEND: !=0 AND <4
+
 import pymongo
 import datetime
 
@@ -30,7 +33,7 @@ class DBmanager:
         for x in collection.find({}).sort([("date", -1)]).limit(1):
             sup.append(x)
         date = x['date']
-        for x in collection.find({"date": date}, {"_id": 0, "date": 1, "report": 1}):
+        for x in collection.find({"date": date}, {"_id": 0, "date": 1, "report": 1,"name": 1, "valutazione": 1}):
             lista.append(x)
         return lista
 
@@ -53,7 +56,7 @@ class DBmanager:
     ################################## HOME #################################################
 
     # return year(x) average reports
-    def retrieve_year_average(self, collection, year):
+    def retrieve_year_average(self, collection, year, range):
         # TODO: ritornare tutti i report medi dell'anno x ogni sito
         #Attualmente: Nel db ho inserito tutte macchine, la media è sull'anno all'interno dell'oggetto obj
         lista = []
@@ -61,54 +64,181 @@ class DBmanager:
             {
                 "$project": {
                     "year": {"$year": "$date"},
-                    "average": "$obj.year",
-                    "date": 1
+                    "name":1,
+                    "average": "$valutazione",
                 }
             },
-            {"$match": { "year": year}},
+            {"$match": {"year": year}},
             {
-        "$group": {
-            "_id": "mediaAnnoZ",
-            "avgValue": {"$avg": "$average"},
-        }
-            }
+                "$group": {
+                    "_id": "$name",
+                    "valutazione": {"$avg": "$average"},
+                    "year": {"$first": year},
+                }
+            }, {"$addFields": {
+                "valutazione": {"$trunc": ["$valutazione", 2]},
+            }},
+            {"$match": {"valutazione": {"$gte": range[0], "$lte": range[1]}}}
         ]):
             lista.append(z)
+        return lista
 
     # return month(x) of the year(x) average reports
-    def retrieve_month_year_average(self, collection, year, month):
+    def retrieve_month_year_average(self, collection, year, month, range):
         # TODO: ritornare tutti i report medi del mese x dell'anno x per ogni sito
-        #Attualmente: Nel db ho inserito tutte macchine, la media è sull'anno all'interno dell'oggetto obj
+        #Ci sono due match perchè se il controllo del range sarebbe stato nel primo, nel calcolo della media avrebbe escluso i campi fuori dal range
+        #In questo modo, invece, escludo la valutazione media
         lista = []
         for z in collection.aggregate([
             {
                 "$project": {
-                    "month": {"$month": "$date"},
                     "year": {"$year": "$date"},
-                    "average": "$obj.year",
-                    "date": 1
+                    "month": {"$month": "$date"},
+                    "name": 1,
+                    "average": "$valutazione",
                 }
             },
-            {"$match": {"month": month, "year": year}},
+            {"$match": {"year": year, "month": month}},
             {
                 "$group": {
-                    "_id": "mediaMeseXAnnoZ",
-                    "avgValue": {"$avg": "$average"},
+                    "_id": "$name",
+                    "valutazione": {"$avg": "$average"},
+                    "year": {"$first": year},
+                    "month": {"$first": month}
                 }
-            }
+            }, {"$addFields": {
+                "valutazione": {"$trunc": ["$valutazione", 2]},
+            }},
+            {"$match": {"valutazione": {"$gte": range[0], "$lte": range[1]}}}
         ]):
             lista.append(z)
-        pass
+        return lista
 
     # return day(x) of month(x) of the year(x) average reports
-    def retrieve_day_month_year_average(self, collection, year, month, day):
+    def retrieve_day_month_year(self, collection, year, month, day,range):
         # TODO: ritornare tutti i report del giorno x del mese x dell'anno x per ogni sito
         lista = []
-        x = datetime.date(year, month, day)
-        #datetime.date non si può salvare in pymongo, quindi nell'oggetto Date abbiamo anche l'orario
-        for z in collection.find({"date": {'$gte': x, '$lt': x + datetime.timedelta(days=1)}},
-                         {"_id": 0, "date": 1, "report": 1}):
+        for z in collection.aggregate([
+            {
+                "$project": {
+                    "year": {"$year": "$date"},
+                    "month": {"$month": "$date"},
+                    "day": {"$dayOfMonth": "$date"},
+                    "name": 1,
+                    #"valutazione": {"$cond": {"if": { "$and": [{"$ne": [range[0], 0]}, {"$ne": [range[1], 0]}, {"$lt": [range[0], 4]},{"$lt": [range[1], 4]}]},"then": {"$cond": {"if": {"$and": [{"$gte": ["$valutazione", range[0]]}  {"$lte": ["$valutazione", range[1]]}]},"then": "$valutazione", "else": None}},    "else": None}},
+                    "valutazione": "$valutazione",
+                    "report": 1
+                }
+            },
+            {"$match": {"year": year, "month": month, "day": day, "valutazione": {"$gte": range[0], "$lte": range[1]}}},
+            {
+                "$group": {
+                    "_id": "$name",
+                    "valutazione": {"$first": "$valutazione"},
+                    "year": {"$first": year},
+                    "month": {"$first": month},
+                    "day": {"$first": day},
+                    "report": {"$first": "$report"}
+
+                }
+            }
+
+        ]):
             lista.append(z)
-        #for x in collection.find({"date": x}, {"_id": 0, "date": 1, "report": 1}):
-         #   lista.append(x)
         return lista
+
+        # return year(x) average reports
+
+    def retrieve_year_average_name(self, collection, year, name,range):
+        # TODO: ritornare tutti i report medi dell'anno x ogni sito
+        # Attualmente: Nel db ho inserito tutte macchine, la media è sull'anno all'interno dell'oggetto obj
+        lista = []
+        for z in collection.aggregate([
+            {
+                "$project": {
+                    "year": {"$year": "$date"},
+                    "name": 1,
+                    "average": "$valutazione",
+                }
+            },
+            {"$match": {"year": year,"name":name}},
+            {
+                "$group": {
+                    "_id": "$name",
+                    "valutazione": {"$avg": "$average"},
+                    "year": {"$first": year},
+                }
+            }, {"$addFields": {
+                "valutazione": {"$trunc": ["$valutazione", 2]},
+            }},
+            {"$match": {"valutazione": {"$gte": range[0], "$lte": range[1]}}}
+        ]):
+            lista.append(z)
+        return lista
+
+        # return month(x) of the year(x) average reports
+
+    def retrieve_month_year_average_name(self, collection, year, month,name,range):
+        # TODO: ritornare tutti i report medi del mese x dell'anno x per ogni sito
+        # Ci sono due match perchè se il controllo del range sarebbe stato nel primo, nel calcolo della media avrebbe escluso i campi fuori dal range
+        # In questo modo, invece, escludo la valutazione media
+        lista = []
+        for z in collection.aggregate([
+            {
+                "$project": {
+                    "year": {"$year": "$date"},
+                    "month": {"$month": "$date"},
+                    "name": 1,
+                    "average": "$valutazione",
+                }
+            },
+            {"$match": {"year": year, "month": month,"name":name}},
+            {
+                "$group": {
+                    "_id": "$name",
+                    "valutazione": {"$avg": "$average"},
+                    "year": {"$first": year},
+                    "month": {"$first": month}
+                }
+            }, {"$addFields": {
+                "valutazione": {"$trunc": ["$valutazione", 2]},
+            }},
+            {"$match": {"valutazione": {"$gte": range[0], "$lte": range[1]}}}
+        ]):
+            lista.append(z)
+        return lista
+
+        # return day(x) of month(x) of the year(x) average reports
+
+    def retrieve_day_month_year_name(self, collection, year, month, day,name,range):
+        # TODO: ritornare tutti i report del giorno x del mese x dell'anno x per ogni sito
+        lista = []
+        for z in collection.aggregate([
+            {
+                "$project": {
+                    "year": {"$year": "$date"},
+                    "month": {"$month": "$date"},
+                    "day": {"$dayOfMonth": "$date"},
+                    "name": 1,
+                    # "valutazione": {"$cond": {"if": { "$and": [{"$ne": [range[0], 0]}, {"$ne": [range[1], 0]}, {"$lt": [range[0], 4]},{"$lt": [range[1], 4]}]},"then": {"$cond": {"if": {"$and": [{"$gte": ["$valutazione", range[0]]}  {"$lte": ["$valutazione", range[1]]}]},"then": "$valutazione", "else": None}},    "else": None}},
+                    "valutazione": "$valutazione",
+                    "report": 1
+                }
+            },
+            {"$match": {"year": year, "month": month, "day": day, "valutazione": {"$gte": range[0], "$lte": range[1]},"name":name}},
+            {
+                "$group": {
+                    "_id": "$name",
+                    "valutazione": {"$first": "$valutazione"},
+                    "year": {"$first": year},
+                    "month": {"$first": month},
+                    "day": {"$first": day},
+                    "report": {"$first": "$report"}
+
+                }
+            }
+
+        ]):
+            lista.append(z)
+        return lista
+
